664dcfcec82879116fdc4b6e9bb12e6d0bb453ff
DangerZone3574 <galcovinyas@gmail.com>
2024-01-18 21:02:47 +0100

    Action tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const labels = core.getInput('labels');
+  const labels = github.context.payload?.pull_request?.labels;
   core.setOutput("newlabels", labels);
 } catch (error) {
   core.setFailed(error.message);
